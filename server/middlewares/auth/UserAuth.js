0db9eaf28c23a20fc23c7d0e254e58aa40883237
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-03-23 13:25:35 +0700

    initialize express and update package.json

---
+++ server/middlewares/auth/UserAuth.js
@@ -0,0 +1,32 @@
+var User = require('../models/User')
+var jwt = require('jsonwebtoken')
+var UserAuth = {}
+
+UserAuth.isAuth = function (req, res, next) {
+  jwt.verify(req.headers.authentication, 'danangtamtomo', function (err, decoded) {
+    if (err) {
+      res.send({
+        'status': err
+      })
+    } else {
+      next()
+    }
+  })
+}
+
+UserAuth.profileAuth = function (req, res, next) {
+  jwt.verify(req.headers.authentication, 'danangtamtomo', function (err, decoded) {
+    models.User.findById(decoded.id)
+      .then(function (user) {
+      if (user.role === 'admin') {
+        next()
+      } else {
+        res.send({
+          status: 'You should be admin to do this action'
+        })
+      }
+    })
+  })
+}
+
+module.exports = auths
